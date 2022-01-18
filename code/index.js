39e723706be6712afa6971ecd2e3140c58243add
daridev <hernandezdarifrancisco@gmail.com>
2022-01-18 11:39:24 -0600

    jump scroll

---
+++ code/index.js
@@ -0,0 +1,37 @@
+var scrollableElement = document.body;
+let windows_height = window.screen.height;
+let current_screen = 1
+let max_screens = 3
+scrollableElement.addEventListener('wheel', checkScrollDirection);
+
+function checkScrollDirection(event) {
+    if (checkScrollDirectionIsUp(event)) {
+        // Scroll up
+        
+        // Update screen counter
+        current_screen -= 1
+        if (current_screen < 1) {
+            current_screen = 1
+        }
+    } else {
+        // Scroll down
+        
+        // Update screen counter
+        current_screen += 1
+        if (current_screen > max_screens) {
+            current_screen = max_screens
+        }
+    }
+
+    console.log (current_screen)
+
+    // Made scroll
+    window.scroll(0, windows_height * (current_screen - 1))
+  }
+  
+function checkScrollDirectionIsUp(event) {
+    if (event.wheelDelta) {
+        return event.wheelDelta > 0;
+    }
+    return event.deltaY < 0;
+}

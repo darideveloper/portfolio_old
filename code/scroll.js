b01d98d215aa758c472a550852605c0b25885de2
daridev <hernandezdarifrancisco@gmail.com>
2022-01-22 18:53:38 -0600

    header: hover effects and go to sections

--- code/scroll.js
+++ code/scroll.js
@@ -1,6 +1,9 @@
 const scrollableElement = document.body;
 const scroll_button_up = document.querySelector ("main .scroll .up")
 const scroll_button_down = document.querySelector ("main .scroll .down")
+const header_logo = document.querySelector ("header .logo")
+const header_projects = document.querySelector ("header .menu ul a:nth-child(1)")
+const header_contact = document.querySelector ("header .menu ul a:nth-child(2)")
 let current_screen = 1
 let max_screens = 3
 
@@ -17,7 +20,7 @@ scrollableElement.addEventListener('wheel', function (event) {
 })
 
 scroll_button_up.addEventListener ("click", function (e) {
-    // Click in scroll up button
+    // Click in scroll up button: update counters and screen
     if ( !scroll_button_up.classList.contains ("transparent")) {
         decress_screen_counter ()
     }
@@ -25,13 +28,37 @@ scroll_button_up.addEventListener ("click", function (e) {
 })
 
 scroll_button_down.addEventListener ("click", function (e) {
-    // Click in scroll up button
+    // Click in scroll down button: update counters and screen
     if ( !scroll_button_down.classList.contains ("transparent")) {
         incress_screen_counter ()
         change_screen ()
         console.log (!scroll_button_down.classList.contains ("transparent"))
     }
 })
+
+header_logo.addEventListener ("click", function (e) {
+    go_contact ()
+})
+
+header_projects.addEventListener ("click", function (e) {
+    go_projects ()    
+})
+
+header_contact.addEventListener ("click", function (e) {
+    go_contact ()
+})
+
+function go_contact () {
+    // Update screen when go to contact form
+    current_screen = 3
+    update_scroll_buttons ()
+}
+
+function go_projects () {
+    current_screen = 2
+    update_scroll_buttons ()
+}
+
 
 function decress_screen_counter () {
     // Update screen counter
@@ -70,10 +97,12 @@ function update_scroll_buttons () {
 
     if (current_screen == 1) {
         scroll_button_up.classList.add ("transparent")
+        scroll_button_down.classList.remove ("transparent")
     } else if (current_screen == 2) {
         scroll_button_up.classList.remove ("transparent")
         scroll_button_down.classList.remove ("transparent")
     } else if (current_screen == 3) {
+        scroll_button_up.classList.remove ("transparent")
         scroll_button_down.classList.add ("transparent")
     }
 }

a151a24797e80a667d40422b8f5f458279e2ae26
daridev <hernandezdarifrancisco@gmail.com>
2022-01-28 17:06:20 -0600

    animations - fade in fixed elements

--- code/animations.js
+++ code/animations.js
@@ -1,9 +1,41 @@
+const anim_logo = document.querySelector ("header .logo")
+const anim_socials = document.querySelector ("main .socials")
+const anim_menu = document.querySelector ("header .menu")
+const anim_scroll = document.querySelector ("main .scroll")
+
 function sleep(s) {
     return new Promise(resolve => setTimeout(resolve, s*1000));
 }
 
+function get_display (elem) {
+    display = elem.computedStyleMap().get("display")["value"]
+    return display
+}
+
 async function anim_fixed_elems () {
-    asyncconsole.log ("fixed done")
+    await sleep(0.5)
+
+    if (get_display (anim_logo) != "none") {
+        await sleep(.25/2)
+        anim_logo.classList.remove ("fade-in")
+    }
+
+    if (get_display (anim_menu) != "none") {
+        await sleep(.25/2)
+        anim_menu.classList.remove ("fade-in")
+    }
+
+    if (get_display (anim_socials) != "none") {
+        await sleep(.25/2)
+        anim_socials.classList.remove ("fade-in")
+    }
+
+
+    if (get_display (anim_scroll) != "none") {
+        await sleep(.25/2)
+        anim_scroll.classList.remove ("fade-in")
+    }
+    
 }
 
 async function anim_fade_out () {
@@ -23,3 +55,6 @@ async function anim_fade_in () {
     await sleep (0.5)
     console.log ("done")
 }
+
+
+anim_fixed_elems ()

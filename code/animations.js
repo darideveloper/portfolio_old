413e00a0c99e13b8850a641f6fafcd5d18f57c8d
daridev <hernandezdarifrancisco@gmail.com>
2022-01-29 01:33:39 -0600

    animations - projects fade

--- code/animations.js
+++ code/animations.js
@@ -9,6 +9,15 @@ const elem_main_up = document.querySelector("main .iam .up")
 const elem_main_title = document.querySelector("main .iam .title")
 const elem_main_down = document.querySelector("main .iam .down")
 
+// Projects elements
+const elem_projects_imgs = document.querySelector(".projects .wrapper-images")
+const elem_projects_title = document.querySelector(".projects .info .wrapper-title")
+const elem_projects_text = document.querySelector(".projects .info .details")
+const elem_projects_cta = document.querySelector(".projects .info .cta")
+
+// Animations time
+wait_time = 0.5
+
 // Animations initial values for fixed elements
 elem_logo.style.opacity = "0"
 elem_socials.style.opacity = "0"
@@ -29,21 +38,32 @@ elem_main_up.style.transform = "translateX(-50px)"
 elem_main_down.style.transform = "translateX(50px)"
 elem_main_title.style.transform = "translateY(50px)"
 
+// Animations initial values for projects
+elem_projects_imgs.style.opacity = "0"
+elem_projects_title.style.opacity = "0"
+elem_projects_text.style.opacity = "0"
+elem_projects_cta.style.opacity = "0"
+
+elem_projects_imgs.style.transform = "translateX(-50px)"
+elem_projects_title.style.transform = "translateX(50px)"
+elem_projects_text.style.transform = "translateX(-50px)"
+elem_projects_cta.style.transform = "translateX(50px)"
+
 
 function sleep(s) {
     return new Promise(resolve => setTimeout(resolve, s*1000));
 }
 
 function get_display (elem) {
-    display = elem.computedStyleMap().get("display")["value"]
+    display = document.defaultView.getComputedStyle(elem)["display"]
     return display
 }
 
 async function anim_fixed_elems () {
-    await sleep(0.5)
+    await sleep(wait_time)
 
     if (get_display (elem_logo) != "none") {
-        await sleep(.25/2)
+        await sleep(wait_time/4)
         anime({
             targets: [elem_logo],
             opacity: [0, 1],
@@ -52,7 +72,7 @@ async function anim_fixed_elems () {
     }
 
     if (get_display (elem_menu) != "none") {
-        await sleep(.25/2)
+        await sleep(wait_time/4)
         anime({
             targets: elem_menu,
             opacity: 1,
@@ -61,7 +81,7 @@ async function anim_fixed_elems () {
     }
 
     if (get_display (elem_socials) != "none") {
-        await sleep(.25/2)
+        await sleep(wait_time/4)
         anime({
             targets: elem_socials,
             opacity: 1,
@@ -70,7 +90,7 @@ async function anim_fixed_elems () {
     }
 
     if (get_display (elem_scroll) != "none") {
-        await sleep(.25/2)
+        await sleep(wait_time/4)
         anime({
             targets: elem_scroll,
             opacity: 1,
@@ -86,7 +106,7 @@ async function anim_fade_out () {
     if (last_screen == 1) {
         await fade_out_main ()
     } else if (last_screen == 2) {
-        console.log ("out projects") 
+        await fade_out_projects ()
     } else if (last_screen == 3) {
         console.log ("out contact")
     }
@@ -96,26 +116,23 @@ async function anim_fade_out () {
 
 async function anim_fade_in () {
     if (current_screen == 1) {
-        console.log ("in main")
         await fade_in_main ()
     } else if (current_screen == 2) {
-        console.log ("out projects") 
+        await fade_in_projects ()
     } else if (current_screen == 3) {
         console.log ("out contact")
     }
-
-    restart_displays ()
 }
 
 async function fade_in_main () {
-    await sleep(.5)
+    await sleep(wait_time)
     anime({
         targets: elem_main_title,
         opacity: 1,
         translateY: 0
     });
 
-    await sleep(.5)
+    await sleep(wait_time)
     anime({
         targets: [elem_main_up, elem_main_down],
         opacity: 1,
@@ -124,7 +141,7 @@ async function fade_in_main () {
 }
 
 async function fade_out_main () {
-    await sleep(.5)
+    await sleep(wait_time)
     anime({
         targets: elem_main_up,
         opacity: 0,
@@ -137,12 +154,99 @@ async function fade_out_main () {
         translateX: 50,
     });
     
-    await sleep(.5)
+    await sleep(wait_time)
     anime({
         targets: elem_main_title,
         opacity: 0,
         translateY: 50,
     });
+}
+
+let anim_projects_imgs = anime({
+    targets: elem_projects_imgs,
+    opacity: [0, 1],
+    translateX: [-50, 0],
+    autoplay: false,
+    duration: 1000
+});
+
+let anim_projects_title = anime({
+    targets: elem_projects_title,
+    opacity: [0, 1],
+    translateX: [50, 0],
+    autoplay: false,
+    duration: 1000
+});
+
+let anim_projects_text = anime({
+    targets: elem_projects_text,
+    opacity: [0, 1],
+    translateX: [-50, 0],
+    autoplay: false,
+    duration: 1000
+});
+
+let anim_projects_cta = anime({
+    targets: elem_projects_cta,
+    opacity: [0, 1],
+    translateX: [-50, 0],
+    autoplay: false,
+    duration: 1000
+});
+
+async function fade_in_projects () {
+
+    await sleep(wait_time)
+    anim_projects_imgs.play()
+
+    await sleep(wait_time)
+    anim_projects_title.play()
+
+    if (get_display (elem_projects_text) != "none") { 
+        await sleep(wait_time)
+        anim_projects_text.play()
+    }
+
+    await sleep(wait_time)
+    anim_projects_cta.play()
+
+    // Restart tyle attribute
+    elem_projects_imgs.setAttribute ("style", "")
+    elem_projects_title.setAttribute ("style", "")
+    elem_projects_text.setAttribute ("style", "")
+    elem_projects_cta.setAttribute ("style", "")
+
+}
+
+async function fade_out_projects () {
+    
+    // Invert animations
+    anim_projects_imgs.reverse()
+    anim_projects_title.reverse()
+    anim_projects_text.reverse()
+    anim_projects_cta.reverse()
+
+    await sleep(wait_time)
+    anim_projects_imgs.play()
+
+    await sleep(wait_time)
+    anim_projects_title.play()
+
+    if (get_display (elem_projects_text) != "none") { 
+        await sleep(wait_time)
+        anim_projects_text.play()
+    }
+
+    await sleep(wait_time)
+    anim_projects_cta.play()
+
+    
+    // Rset animations state
+    await sleep(wait_time*4)
+    anim_projects_imgs.reverse()
+    anim_projects_title.reverse()
+    anim_projects_text.reverse()
+    anim_projects_cta.reverse()
 }
 
 anim_fixed_elems ()
